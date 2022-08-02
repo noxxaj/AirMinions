e78769c4f3f25ab7315d70f4d70edbc5bfad5ec2
jaxxon alta <jaxxonalta@gmail.com>
2022-08-02 21:11:40 +0200

    minion#show action created

--- app/controllers/minions_controller.rb
+++ app/controllers/minions_controller.rb
@@ -1,16 +1,20 @@
-class MinionController < ApplicationController
-  # before_action :set_minion, only: [:edit, :show]
+class MinionsController < ApplicationController
+  before_action :set_minion, only: %i[show]
   def index
-    @minions = Minion.all
+    @minions = policy_scope(Minion).order(created_at: :desc)
   end
 
   def create
     @minion = Minion.new(minion_params)
     if @minion.save
       redirect_to minions_path
-    # else
-      # render :new
+    else
+      render :new
     end
+  end
+
+  def show
+    authorize @minion
   end
 
   private
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,6 +5,8 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+User.create!(username: "fake_user123", email: "fake@gmail.com", password: "iloveminions")
+
 5.times do
-  Minion.create!(name: "Fred", skills: "Digging")
+  Minion.create!(name: "Fred", skills: "Digging", user_id: 1, price_per_day: 10_000)
 end
